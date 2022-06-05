5bc6ddaca9e61e55eba38afebdad36277bc00ffe
saivenkat772 <102047118+saivenkat772@users.noreply.github.com>
2022-06-05 23:42:35 +0530

    Add files via upload

---
+++ permutations.py
@@ -0,0 +1,10 @@
+"""https://cses.fi/problemset/task/1070"""
+n=int(input())
+if n==2 or n==3:
+    print("NO SOLUTION")
+else:
+    l=[]
+    for i in range(2,n+1,2):
+        print(i,end=" ")
+    for i in range(1,n+1,2):
+        print(i,end=" ")
