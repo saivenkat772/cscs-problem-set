e89fbec3543e926ea375651cc5dcb4490502934c
saivenkat772 <102047118+saivenkat772@users.noreply.github.com>
2022-06-05 23:28:55 +0530

    Update repetitions.py

--- repetitions.py
+++ repetitions.py
@@ -1,12 +1,15 @@
 """https://cses.fi/problemset/task/1069"""
 s=input()
-m=0
-co=0
-for i in range(len(s)-1):
-    if s[i]==s[i+1]:
-        co+=1
-    else:
-        co=1
-    if co>m:
-        m=co 
-print(m)
+if len(s)==1:
+    print(1)
+else:
+    m=0
+    co=1
+    for i in range(len(s)-1):
+        if s[i]==s[i+1]:
+            co+=1
+        else:
+            co=1
+        if co>m:
+            m=co 
+    print(m)
